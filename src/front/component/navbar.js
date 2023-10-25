e4b1ea1bbbe43595e52ffb448d3651817a6f684b
Sofia Fernandes <123645624+sofiafernands@users.noreply.github.com>
2023-10-25 16:27:08 +0000

    nav ok

--- src/front/component/navbar.js
+++ src/front/component/navbar.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import { MdEmail } from 'react-icons/md';
+import { FaPhone } from 'react-icons/fa';
+
+
+
+
 
 function Navbar() {
   const [expanded, setExpanded] = useState(false);
@@ -20,13 +28,20 @@ function Navbar() {
         <div className={`collapse navbar-collapse ${expanded ? 'show' : ''}`}>
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 text-white">
             <li className="nav-item">
-              <a className="nav-link active text-white" aria-current="page" href="#">
+              <a className="nav-link active text-white" aria-current="page" href="services">
                 Servicios
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-white" href="#">
+              <a className="nav-link text-white" href="mailto:info@davidriphones.com">
+              <MdEmail className="me-2" size={24} />
                 Contacto
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="tel:692982400">
+              <FaPhone className="me-2" size={24} />
+                Tlf
               </a>
             </li>
             <li className="nav-item">
@@ -37,7 +52,20 @@ function Navbar() {
           </ul>
         </div>
         <section className="d-flex">
-          <input type='text' className='me-2 rounded small ' placeholder='reparacion por modelo' />
+        <div className="d-flex align-items-center">
+            <div className="input-group">
+              <span className="input-group-text">
+                <FontAwesomeIcon icon={faSearch} />
+              </span>
+              <input
+                type="text"
+                className="form-control small"
+                placeholder="Reparación por modelo"
+                aria-label="Reparación por modelo"
+              />
+            </div>
+          </div>
+          <FontAwesomeIcon icon={faUser} className='d-flex m-2 align-items-center'/>
           <button className="btn btn btn-dark me-2" type="submit">
             Iniciar sesión
           </button>
